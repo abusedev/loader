5e6d6314f375dc1bab9e5adb686e390fbf071049
romance <66000596+romancewastaken@users.noreply.github.com>
2024-03-27 15:35:51 -0400

    Add files via upload

---
+++ handler/console/consoleWriter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace abuseloader.handler.console
+{
+    internal class consoleWriter
+    {
+        public static int options = 0;
+
+        public static void writeLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            consoleManager.centerText("        __                                ");
+            consoleManager.centerText(@"       /\ \                               ");
+            consoleManager.centerText(@"   __  \ \ \____  __  __    ____     __   ");
+            consoleManager.centerText(@" /'__`\ \ \ '__`\/\ \/\ \  /',__\  /'__`\ ");
+            consoleManager.centerText(@"/\ \L\.\_\ \ \L\ \ \ \_\ \/\__, `\/\  __/ ");
+            consoleManager.centerText(@"\ \__/.\_\\ \_,__/\ \____/\/\____/\ \____\");
+            consoleManager.centerText(@" \/__/\/_/ \/___/  \/___/  \/___/  \/____/");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        public static void optionsWriter(string text)
+        {
+            options++;
+            Console.WriteLine($"{options}.) {text}");
+        }
+
+        public static void resetOptions()
+        {
+            options = 0;
+        }
+    }
+}
